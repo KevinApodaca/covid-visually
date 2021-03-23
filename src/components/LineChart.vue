8693158ef3e31421e9a18be9c05a4c11fc492e64
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-03-23 13:56:39 -0600

    :sparkles: Handling Chart data

---
+++ src/components/LineChart.vue
@@ -0,0 +1,32 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: line,
+  props: {
+    label: {
+      type: String
+    },
+    chartData: {
+      type: Array
+    },
+    options: {
+      type: Object
+    }
+  },
+  mounted () {
+    const dates = this.chartData.map(d => d.date).reverse()
+    const totals = this.chartData.map(d => d.total).reverse()
+
+    this.renderChart({
+      labels: dates,
+      dataset: [{
+        label: this.label,
+        data: totals,
+      }]
+    },
+    this.options
+    )
+  }
+}
+</script>
