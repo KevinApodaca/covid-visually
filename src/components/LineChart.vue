28b58133bea0dcbbdb4c4729b7969d6b0ddb2c7c
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-03-23 14:57:53 -0600

    :sparkles: charts can now customize colors

--- src/components/LineChart.vue
+++ src/components/LineChart.vue
@@ -12,17 +12,27 @@ export default {
     },
     options: {
       type: Object
+    },
+    chartColors: {
+      type: Object
     }
+
   },
   mounted () {
     const dates = this.chartData.map(d => d.date).reverse()
     const totals = this.chartData.map(d => d.total).reverse()
+
+    const { borderColor, pointBorderColor, pointBackgroundColor, backgroundColor } = this.chartColors
 
     this.renderChart({
       labels: dates,
       datasets: [{
         label: this.label,
         data: totals,
+        borderColor: borderColor,
+        pointBorderColor: pointBorderColor,
+        pointBackgroundColor: pointBackgroundColor,
+        backgroundColor: backgroundColor
       }]
     },
     this.options
