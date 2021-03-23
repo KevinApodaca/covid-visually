e7341836b5c993a7135c440ed029514464eb199f
Kevin Apodaca <kevin.apodaca7@gmail.com>
2021-03-23 13:35:28 -0600

    :globe_with_meridians: initial api request

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  async created() {
+    const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json')
+    console.log(data)
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
